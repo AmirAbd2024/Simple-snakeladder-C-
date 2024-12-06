3ca966f047025b85e4ce5db979d058451793cca7
Amir <184638235+AmirAbd2024@users.noreply.github.com>
2024-12-06 11:19:12 +0330

    Add files via upload

---
+++ SnakeAndLadder/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace SnakeAndLadder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.playing2 = new System.Windows.Forms.Button();
+            this.playing1 = new System.Windows.Forms.Button();
+            this.ChanceBTN = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::SnakeAndLadder.Properties.Resources.snakeladderback;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.playing2);
+            this.panel1.Controls.Add(this.playing1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(780, 600);
+            this.panel1.TabIndex = 0;
+            // 
+            // playing2
+            // 
+            this.playing2.BackColor = System.Drawing.Color.Blue;
+            this.playing2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.playing2.Location = new System.Drawing.Point(26, 555);
+            this.playing2.Name = "playing2";
+            this.playing2.Size = new System.Drawing.Size(30, 25);
+            this.playing2.TabIndex = 2;
+            this.playing2.Text = "2";
+            this.playing2.UseVisualStyleBackColor = false;
+            // 
+            // playing1
+            // 
+            this.playing1.BackColor = System.Drawing.Color.Red;
+            this.playing1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.playing1.Location = new System.Drawing.Point(26, 555);
+            this.playing1.Name = "playing1";
+            this.playing1.Size = new System.Drawing.Size(30, 25);
+            this.playing1.TabIndex = 1;
+            this.playing1.Text = "1";
+            this.playing1.UseVisualStyleBackColor = false;
+            // 
+            // ChanceBTN
+            // 
+            this.ChanceBTN.Location = new System.Drawing.Point(828, 425);
+            this.ChanceBTN.Name = "ChanceBTN";
+            this.ChanceBTN.Size = new System.Drawing.Size(69, 54);
+            this.ChanceBTN.TabIndex = 3;
+            this.ChanceBTN.Text = "Dice";
+            this.ChanceBTN.UseVisualStyleBackColor = true;
+            this.ChanceBTN.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(825, 295);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label2.Location = new System.Drawing.Point(853, 482);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 29);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(927, 609);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ChanceBTN);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SnakeLadder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button playing1;
+        private System.Windows.Forms.Button ChanceBTN;
+        private System.Windows.Forms.Button playing2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ SnakeAndLadder/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SnakeAndLadder
+{
+    public partial class Form1 : Form
+    {
+        byte player1LOC = 1, player2LOC = 1;
+        bool IsPlayer1 = true;
+        char[] mapType;
+        byte[] mapDes;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            configMapArray();
+            configGraph();
+            IsPlayer1 = false;
+            graphLocation(1, true);
+            IsPlayer1 = true;
+            graphLocation(1, true);
+        }
+
+
+        void graphLocation(byte number, bool IsDirectDest)
+        {
+            byte tmpLocVal = 1;
+            ChanceBTN.Enabled = false; 
+
+            if (!IsDirectDest)
+            {
+                if (IsPlayer1) { player1LOC += number; tmpLocVal = player1LOC; }
+                else { player2LOC += number; tmpLocVal = player2LOC; }
+            }
+            else
+            {
+                if (IsPlayer1) { player1LOC = number; tmpLocVal = player1LOC; }
+                else { player2LOC = number; tmpLocVal = player2LOC; }
+            }
+
+
+            int locY = (tmpLocVal % 10 != 0) ? panel1.Height - ((tmpLocVal / 10) * (panel1.Height / 10)) - (panel1.Height / 10 * 3 / 4) : panel1.Height - (((tmpLocVal / 10) - 1) * (panel1.Height / 10)) - (panel1.Height / 10 * 3 / 4);
+            int locX = (tmpLocVal % 10 != 0) ? (((tmpLocVal % 10) - 1) * (panel1.Width / 10)) + ((panel1.Width / 10) / 3) : panel1.Width - ((panel1.Width / 10) * 3 / 4);
+
+
+            if (tmpLocVal > 100 && IsPlayer1) { MessageBox.Show("Dice value is over 100 for player 1"); player1LOC -= number; ChanceBTN.Enabled = true; }
+            else if (tmpLocVal > 100 && !IsPlayer1) { MessageBox.Show("Dice value is over 100 for player 2"); player2LOC -= number; ChanceBTN.Enabled = true; }
+            else if (tmpLocVal == 100 && IsPlayer1) { playing1.Location = new Point(locX, locY); MessageBox.Show("Player 1 is winner"); ChanceBTN.Enabled = false; }
+            else if (tmpLocVal == 100 && !IsPlayer1) { playing2.Location = new Point(locX, locY); MessageBox.Show("Player 2 is winner"); ChanceBTN.Enabled = false; }
+            else
+            {
+
+                if (IsPlayer1) { playing1.Location = new Point(locX, locY); label1.Text = "Player 1 :  " + player1LOC; ChanceBTN.Enabled = true; }
+                else { playing2.Location = new Point(locX, locY); label1.Text = "Player 2 :  " + player2LOC; ChanceBTN.Enabled = true; }
+
+                for (int i = 0; i < 101; i++)
+                {
+                    if (mapType[tmpLocVal] != 'e') { ChanceBTN.Enabled = false; MessageBox.Show(((mapType[tmpLocVal] == 's') ? "Snake to  " : "Jump to  ") + mapDes[tmpLocVal]); graphLocation(mapDes[tmpLocVal], true); break; }
+                }
+            }
+        }
+
+
+        void check4SameCell()
+        {
+            if (player1LOC == player2LOC)
+            {
+                MessageBox.Show((IsPlayer1) ? "Player 1 sent player 2 to start point." : "Player 2 sent player 1 to start point.");
+                IsPlayer1 = !IsPlayer1;
+                graphLocation(1, true);
+                IsPlayer1 = !IsPlayer1;
+            }
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            byte CHVAL = Convert.ToByte(rnd.Next(1, 7));
+            label2.Text = CHVAL.ToString();
+            graphLocation(CHVAL, false);
+            check4SameCell();
+            IsPlayer1 = !IsPlayer1;
+        }
+
+        void configMapArray()
+        {
+            int mapLen = 101;
+            mapType = new char[mapLen];
+            mapDes = new byte[mapLen];
+            for (int i = 0; i < mapLen; i++) { mapType[i] = 'e'; }
+            mapType[3] = 'l'; mapType[6] = 'l'; mapType[20] = 'l'; mapType[36] = 'l'; mapType[63] = 'l'; mapType[68] = 'l';
+            mapDes[3] = 51; mapDes[6] = 27; mapDes[20] = 70; mapDes[36] = 55; mapDes[63] = 95; mapDes[68] = 98;
+            mapType[25] = 's'; mapType[34] = 's'; mapType[47] = 's'; mapType[65] = 's'; mapType[87] = 's'; mapType[91] = 's'; mapType[99] = 's';
+            mapDes[25] = 5; mapDes[34] = 1; mapDes[47] = 19; mapDes[65] = 52; mapDes[87] = 57; mapDes[91] = 61; mapDes[99] = 69;
+        }
+
+        void configGraph()
+        {
+            GraphicsPath gp = new GraphicsPath();
+            gp.AddEllipse(0, 0, playing1.Width, playing1.Height);
+            playing1.Region = new Region(gp);
+            playing2.Region = new Region(gp);
+        }
+
+       
+
+    }
+}
